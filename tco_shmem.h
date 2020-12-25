e52bd2ae3fdd865b4bf338e7c839a4cc06211648
Tomasz Lisowski <t.p.lisowski@gmail.com>
2020-12-26 00:47:28 +0100

    Add shared memory definitions

---
+++ tco_shmem.h
@@ -0,0 +1,37 @@
+#ifndef _TCO_SHMEM_H_
+#define _TCO_SHMEM_H_
+
+#include <stdint.h>
+
+/* Shared memory regions */
+
+/* 
+Store output of the ML model and any related data needed by the control
+server. 
+*/
+#define TCO_SHMEM_NAME_MODEL "tco_shmem_model"
+#define TCO_SHMEM_NAME_SEM_MODEL "tco_shmem_sem_model"
+struct tco_shmem_data_model
+{
+    uint32_t mem_size;
+};
+
+/* 
+Store output of the control server any any related data needed by the io
+server to control the hardware.
+*/
+#define TCO_SHMEM_NAME_CONTROL "tco_shmem_control"
+#define TCO_SHMEM_NAME_SEM_CONTROL "tco_shmem_sem_control"
+struct tco_shmem_data_control
+{
+    uint32_t mem_size;
+    struct
+    {
+        uint16_t pulse_len_min;
+        uint16_t pulse_len_max;
+        uint8_t ch;
+        float turn_frac;
+    } steering;
+};
+
+#endif /* _TCO_SHMEM_H_ */
