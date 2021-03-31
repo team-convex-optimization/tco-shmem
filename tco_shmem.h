edc93d2c64aace3a0b0eb928f77b2266f910508d
Tomasz Lisowski <t.p.lisowski@gmail.com>
2021-03-31 20:34:17 +0200

    Force struct alignment and packing for predictable offsets

--- tco_shmem.h
+++ tco_shmem.h
@@ -14,8 +14,8 @@ control daemon.
 #define TCO_SHMEM_NAME_SEM_PLAN "tco_shmem_sem_plan"
 struct tco_shmem_data_plan
 {
-    uint8_t valid;                         /* =0 means shared memory is invalid, >0 means valid */
-    uint16_t waypts[TCO_PLAN_WAYPTS_N][2]; /* Each element contains x and y coordiantes of the waypoint on a frame */
+    _Alignas(1) uint8_t valid;                         /* =0 means shared memory is invalid, >0 means valid */
+    _Alignas(4) uint16_t waypts[TCO_PLAN_WAYPTS_N][2]; /* Each element contains x and y coordiantes of the waypoint on a frame */
 };
 #define TCO_SHMEM_SIZE_PLAN sizeof(struct tco_shmem_data_plan)
 
@@ -27,12 +27,12 @@ hardware.
 #define TCO_SHMEM_NAME_SEM_CONTROL "tco_shmem_sem_control"
 struct tco_shmem_data_control
 {
-    uint8_t valid;     /* =0 means shared memory is invalid, >0 means valid */
-    uint8_t emergency; /* =0 mean no emergency, >1 means there is an emergency */
-    struct
+    _Alignas(1) uint8_t valid;     /* =0 means shared memory is invalid, >0 means valid */
+    _Alignas(1) uint8_t emergency; /* =0 mean no emergency, >1 means there is an emergency */
+    _Alignas(4) struct
     {
-        uint8_t active; /* =0 means inactive, >0 active */
-        float pulse_frac;
+        _Alignas(4) float pulse_frac;
+        _Alignas(1) uint8_t active; /* =0 means inactive, >0 active */
     } ch[16];
 };
 #define TCO_SHMEM_SIZE_CONTROL sizeof(struct tco_shmem_data_control)
@@ -45,13 +45,13 @@ Store state of the simulated car. This is only used when training hence the ifde
 #define TCO_SHMEM_NAME_SEM_TRAINING "tco_shmem_sem_training"
 struct tco_shmem_data_training
 {
-    uint8_t valid;                                    /* =0 means shared memory is invalid, >0 means valid */
-    uint8_t state;                                    /* =0 means simulator is paused. =1 means request to step once. =2 means request to hard reset. Gym will wait until this is returned back to 0 before proceeding. */
-    uint8_t wheels_off_track[4];                      /* [0] = FL, [1] = FR, [2] = RL, [3] = RR */
-    uint8_t drifting;                                 /* =0 when not drifting and >0 when drifting */
-    float speed;                                      /* meters per second */
-    float pos[3];                                     /* [0] = x, [1] = y, [2] = z */
-    uint8_t video[TCO_FRAME_HEIGHT][TCO_FRAME_WIDTH]; /* Grayscale camera feed from the simulator */
+    _Alignas(1) uint8_t valid;                                    /* =0 means shared memory is invalid, >0 means valid */
+    _Alignas(1) uint8_t state;                                    /* =0 means simulator is paused. =1 means request to step once. =2 means request to hard reset. Gym will wait until this is returned back to 0 before proceeding. */
+    _Alignas(1) uint8_t drifting;                                 /* =0 when not drifting and >0 when drifting */
+    _Alignas(4) float speed;                                      /* meters per second */
+    _Alignas(4) float pos[3];                                     /* [0] = x, [1] = y, [2] = z */
+    _Alignas(4) uint8_t wheels_off_track[4];                      /* [0] = FL, [1] = FR, [2] = RL, [3] = RR */
+    _Alignas(4) uint8_t video[TCO_FRAME_HEIGHT][TCO_FRAME_WIDTH]; /* Grayscale camera feed from the simulator */
 };
 #define TCO_SHMEM_SIZE_TRAINING sizeof(struct tco_shmem_data_training)
 #endif /* TRAINING */
