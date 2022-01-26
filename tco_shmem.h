86687fe7f895e685f98146e449d91ea4fe00f801
Eduardo Lira <edulira@me.com>
2022-01-26 22:29:46 +0100

    Move the sensor data into it's own shmem_object

--- tco_shmem.h
+++ tco_shmem.h
@@ -5,7 +5,7 @@
 #define TCO_FRAME_WIDTH 640U
 #define TCO_FRAME_HEIGHT 480U
 
-/* 
+/*
 Store output of the plan daemon which is a description of the desired path to be taken by the
 control daemon.
 */
@@ -16,11 +16,22 @@ struct tco_shmem_data_plan
     _Alignas(4) float target_pos;   /* Desired position. -1 left edge of frame, 1 right edge of frame, 0 is center.  */
     _Alignas(4) float target_speed; /* Desired speed in meters per second. */
     _Alignas(4) uint32_t frame_id;
-    _Alignas(8) double ultrasound_left;
-    _Alignas(8) double ultrasound_right;
-    _Alignas(8) double hall_effect_rpm;
 };
 #define TCO_SHMEM_SIZE_PLAN sizeof(struct tco_shmem_data_plan)
+
+/*
+Store output of the sensor daemon which stores all of the sensor outputs for the control daemon.
+*/
+#define TCO_SHMEM_NAME_SENSOR "tco_shmem_sensor"
+#define TCO_SHMEM_NAME_SEM_SENSOR "tco_shmem_sem_sensor"
+struct tco_shmem_data_sensor
+{
+    _Alignas(4) uint32_t time_step; /* Similar to ID, increments with every reading of sensord main thread */
+    _Alignas(8) double ultrasound_left;
+    _Alignas(8) double ultrasound_right;
+    _Alignas(8) double hall_effect_rpm; /* RPM, scaled to rear wheel ratio, of the car */
+};
+#define TCO_SHMEM_SIZE_SENSOR sizeof(struct tco_shmem_data_sensor)
 
 /* 
 Store output of the control daemon and any related data needed by the io daemon to control the
@@ -61,19 +72,22 @@ struct tco_shmem_data_state
     {                             \
         TCO_SHMEM_NAME_CONTROL,   \
             TCO_SHMEM_NAME_STATE, \
-            TCO_SHMEM_NAME_PLAN   \
+            TCO_SHMEM_NAME_PLAN,   \
+            TCO_SHMEM_NAME_SENSOR   \
     }
 #define TCO_SHMEM_ARR_SEM_NAME        \
     {                                 \
         TCO_SHMEM_NAME_SEM_CONTROL,   \
             TCO_SHMEM_NAME_SEM_STATE, \
-            TCO_SHMEM_NAME_SEM_PLAN   \
+            TCO_SHMEM_NAME_SEM_PLAN,   \
+            TCO_SHMEM_NAME_SEM_SENSOR   \
     }
 #define TCO_SHMEM_ARR_SIZE        \
     {                             \
         TCO_SHMEM_SIZE_CONTROL,   \
             TCO_SHMEM_SIZE_STATE, \
-            TCO_SHMEM_SIZE_PLAN   \
+            TCO_SHMEM_SIZE_PLAN,   \
+            TCO_SHMEM_SIZE_SENSOR   \
     }
 
 #endif /* _TCO_SHMEM_H_ */
